ff87dc047562a2e07423f4e85102cb0d9cc53347
Jafar Mahin <bsse1109@iit.du.ac.bd>
2022-03-28 14:50:37 +0600

    Added Comments

--- src/code/MainDriver.java
+++ src/code/MainDriver.java
@@ -12,6 +12,15 @@ public class MainDriver {
         int noOfSamples = 20;
 
         ArrayList<BufferedImage> dataset = new ArrayList<>();
+
+        /*
+        Takes input of 20 samples
+
+        Index 1 - 10 images are GREEN
+        Index 11 - 20 images are SNOW
+
+        */
+
         String samplePath = "src/dataset/all_sample_2/s";
 
         for (int i = 1; i <= noOfSamples; i++) {
@@ -20,11 +29,18 @@ public class MainDriver {
             File tempFile = new File(tempSamplePath);
             BufferedImage tempImage = ImageIO.read(tempFile);
             dataset.add(tempImage);
-//            System.out.println(tempSamplePath);
         }
 
+        /*
+
+        Takes test image to find it's nearest neighbor
+
+        test_image 1, 4 -> SNOW
+        test_image 2, 3 -> GREEN
+
+        */
         String testPath = "src/dataset/test/test_image_4.jpg";
-//        testPath = "src/CSE_504/KNN/dataset/all_sample/s3.jpg";
+
         File testFile = new File(testPath);
         BufferedImage testImage = ImageIO.read(testFile);
 
